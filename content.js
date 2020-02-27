e2a1d977dc61f4235ee88e048e7c47b035bc2b5c
Jacob Conner Samar <jcsamar3@gmail.com>
2020-02-26 23:31:22 -0500

    f

--- content.js
+++ content.js
@@ -1,15 +1,45 @@
-alert("Hello from your Chrome extension!")
+webgazer.setRegression('ridge') /* currently must set regression and tracker */
+		    .setTracker('clmtrackr')
+		    .setGazeListener(function(data, clock) {
+		   	if(data!=null)
+		    	console.log(findDomElementGoogle(data.x,data.y));
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) {
-        return;
-    }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(data.x)
-    console.log(data.y)
-    
-    // console.log(elapsedTime); //elapsed time is based on time since begin was called
-}).begin();
+		     //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+		     //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+		    })
+		    .begin()
+		    .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+		    
+		    var width = 320;
+		    var height = 240;
+		    var topDist = '0px';
+		    var leftDist = '0px';
+		    
+		    var setup = function() {
+		        var video = document.getElementById('webgazerVideoFeed');
+		        video.style.display = 'hidden';
+		        video.style.position = 'absolute';
+		        video.style.top = topDist;
+		        video.style.left = leftDist;
+		        video.width = width;
+		        video.height = height;
+		        video.style.margin = '0px';
 
+		        webgazer.params.imgWidth = width;
+		        webgazer.params.imgHeight = height;
+		    };
+
+		    function checkIfReady() {
+		        if (webgazer.isReady()) {
+		            setup();
+		        } else {
+		            setTimeout(checkIfReady, 100);
+		        }
+		    }
+		    setTimeout(checkIfReady,100);
+
+	        window.onbeforeunload = function() {
+		        webgazer.end();
+		    };
+		};
 
