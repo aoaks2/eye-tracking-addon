224c9c667bcd94e22b50a03f588ead60dbb6704c
jsamar1 <jsamar3@gmail.com>
2020-02-26 23:29:00 -0500

    f

--- content.js
+++ content.js
@@ -1,4 +1,15 @@
 alert("Hello from your Chrome extension!")
 
+webgazer.setGazeListener(function(data, elapsedTime) {
+    if (data == null) {
+        return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(data.x)
+    console.log(data.y)
+    
+    // console.log(elapsedTime); //elapsed time is based on time since begin was called
+}).begin();
 
 
